e40e4bd176ec05c48048e9b61b2a18b2ad013f68
Ayush sen <senayush920@gmail.com>
2025-08-29 10:41:48 +0530

    add port in .env

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,11 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(cors({ origin: ["http://localhost:5173"], credentials: true }))
 
-app.listen(3000, () => {
+
+const PORT = process.env.PORT || 3000
+// console.log('port is ',PORT);
+
+app.listen(PORT, () => {
   console.log("Server is running on port 3000")
 })
 
