8c46d8abcee6b1f871348902fffe2043b48f7174
Ayush sen <senayush920@gmail.com>
2025-08-29 11:41:03 +0530

    add cors to hosted url

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ const app = express()
 // to make input as json
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({ origin: ["http://localhost:5173"], credentials: true }))
+app.use(cors({ origin: ["http://localhost:5173", "https://note-sphere-iota.vercel.app/"], credentials: true }))
 
 
 const PORT = process.env.PORT || 3000
